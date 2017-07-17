fa8278cd8b0e5dbb4b1d18f8129a3d36c913b138
CRISTI\Cristi <cristian.lapadus@ulbsibiu.ro>
2017-07-17 12:51:11 +0300

    10 levels, smooth controls, tiledMap background

---
+++ res/basictiles_2.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
+<tileset name="basictiles_2" tilewidth="16" tileheight="16" tilecount="120" columns="8">
+ <image source="basictiles_2.png" width="128" height="240"/>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="blocked" type="bool" value="false"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="blocked" type="bool" value="true"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="blocked" type="bool" value="true"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="91">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="blocked" type="bool" value="true"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="blocked" type="bool" value="true"/>
+   </properties>
+  </objectgroup>
+ </tile>
+</tileset>
